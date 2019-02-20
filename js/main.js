12a457c33af4f81fdf560de7083fd09d62072285
Russ Kennedy <russell.j.kennedy@googlemail.com>
2019-02-20 15:44:31 +0000

    Pinterest-style blog listings added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
 // Main site JS
-function resizeGridItem(item){
-  grid = document.getElementsByClassName("blog-listings")[0]; // Grid
-  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-  rowSpan = Math.ceil((item.querySelector('.blog-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-    item.style.gridRowEnd = "span "+rowSpan;
-}
-
-function resizeAllGridItems(){
-  allItems = document.getElementsByClassName("item");
-  for(x=0;x<allItems.length;x++){
-    resizeGridItem(allItems[x]);
-  }
-}
-
-function resizeInstance(instance){
-	item = instance.elements[0];
-  resizeGridItem(item);
-}
-
-window.onload = resizeAllGridItems();
-window.addEventListener("resize", resizeAllGridItems);
-
-allItems = document.getElementsByClassName("item");
-for(x=0;x<allItems.length;x++){
-  imagesLoaded( allItems[x], resizeInstance);
-}
 
 // Document ready functions
 jQuery(document).ready(function($) {
